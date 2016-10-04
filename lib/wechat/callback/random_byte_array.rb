8d3bc53baa6465d962cd770aa6adef6082edce7a
Topbit Du <topbit.du@gmail.com>
2016-10-04 10:57:27 +0800

    1, Improve the Random Byte Array class for the argument validation.

--- lib/wechat/callback/random_byte_array.rb
+++ lib/wechat/callback/random_byte_array.rb
@@ -1,10 +1,13 @@
 class Wechat::Callback::RandomByteArray
 
+  extend Wechat::Core::Common
+
   LENGTH = 16
 
   def self.create(length)
 
-    raise ArgumentError.new('The length argument is required.') if length.blank?
+    assert_present! :length, length
+    #raise ArgumentError.new('The length argument is required.') if length.blank?
 
     [*0..255].sample length
 
