47cad8238e24e2cc327faf04b0195d95bc12ebcd
Topbit Du <topbit.du@gmail.com>
2016-10-06 01:39:28 +0800

    1, Improve the XML Document class for the argument validation.

--- lib/wechat/callback/xml_document.rb
+++ lib/wechat/callback/xml_document.rb
@@ -1,10 +1,13 @@
 class Wechat::Callback::XmlDocument
 
+  extend Wechat::Core::Common
+
   # 消息加解密 技术方案
   # http://mp.weixin.qq.com/wiki/6/90f7259c0d0739bbb41d9f4c4c8e59a2.html
   def self.load(xml_text)
 
-    raise ArgumentError.new('The xml_text argument is required.') if xml_text.blank?
+    assert_present! :xml_text, xml_text
+    #raise ArgumentError.new('The xml_text argument is required.') if xml_text.blank?
 
     pairs = {}
     Nokogiri::XML(xml_text).xpath('/xml').first.children.each do |element| pairs[element.name] = element.text end
@@ -16,6 +19,7 @@ class Wechat::Callback::XmlDocument
   # http://mp.weixin.qq.com/wiki/6/90f7259c0d0739bbb41d9f4c4c8e59a2.html
   def self.create(pairs)
 
+    #assert_present! :pairs, pairs
     raise ArgumentError.new('The pairs argument is required.') if pairs.blank?
 
     xml = '<xml>'
